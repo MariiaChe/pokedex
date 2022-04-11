cfef5bd40f3d1bad8b43668f7a23d12dab5310cb
Mariia Chernysheva <maria.bookvoed@gmail.com>
2022-04-10 20:27:24 -0400

    chore(app): minor changes

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import Pokedex from '../views';
+import Header from "../components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Pokedex />
+    </>
+  );
+};
+
+
+export default App;
